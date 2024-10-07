1284d67256a6c992bad41f9bfe446b19629e1dfa
szymczys <tomasz.k.szymczyszyn@nokia.com>
2024-10-07 11:05:17 +0200

    Initial commit

---
+++ src/main/java/com/example/szymczys/SzymczysApplication.java
@@ -0,0 +1,13 @@
+package com.example.szymczys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzymczysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzymczysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/szymczys/SzymczysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.szymczys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzymczysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
