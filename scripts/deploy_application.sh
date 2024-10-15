647e95e44a1ff28163d0bfad1d1abd45772a3ad0
szymczys <tomasz.k.szymczyszyn@nokia.com>
2024-10-15 13:23:06 +0200

    creation deploy script sh

--- scripts/deploy_application.sh
+++ scripts/deploy_application.sh
@@ -2,4 +2,7 @@
 FILE_NAME = szymczys-0.0.1-SNAPSHOT.jar
 JAR_PATH=/home/myApp/target/$FILE_NAME
 PID=$(ps -ef | grep "$FILE_NAME" | grep -v grep | awk '{print $2}')
+kill -9 $PID
+echo "Application stopped."
 java -jar "$JAR_PATH"
+echo "New version of the application run."
