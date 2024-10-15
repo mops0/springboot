c984590e5f01bbea864230849c1b2393329e0fe3
szymczys <tomasz.k.szymczyszyn@nokia.com>
2024-10-15 13:41:49 +0200

    creation deploy script sh

--- scripts/deploy_application.sh
+++ scripts/deploy_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FILE_NAME = szymczys-0.0.1-SNAPSHOT.jar
-JAR_PATH=/home/myApp/target/$FILE_NAME
+JAR_PATH=/home/ubuntu/myApp/target/$FILE_NAME
 PID=$(ps -ef | grep "$FILE_NAME" | grep -v grep | awk '{print $2}')
 kill -9 $PID
 echo "Application stopped."
